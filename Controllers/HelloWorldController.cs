c1a65568ac5d4ac304d68afa0d9cb335a2a229ca
MATHEADS\urajkovic <uros.rajkovic@mathema.de>
2022-07-16 00:59:11 +0200

    commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstMVCApp.Models;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel()
+            { Name="SIF", Age=2};
+            return View(doggo);
+        }
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            return View("index");
+        }
+        public string Hello()
+        {
+            return "who is there?";
+        }
+    }
+}
